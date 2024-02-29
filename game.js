b819a56932e881352ee84844e74d523c2bd1ef52
21134lb <124744146+21134lb@users.noreply.github.com>
2024-02-29 15:08:03 +1300

    added obstacle group

--- game.js
+++ game.js
@@ -11,9 +11,9 @@ function setup() {
   cnv = new Canvas('2:1');
   const PLAYERWIDTH = width/18;
   const PLAYERHEIGHT = height/9;
-  const PLAYERSPAWNY = height- PLAYERHEIGHT;
+  const PLAYERSPAWNY = height- PLAYERHEIGHT/2;
   const PLAYERSPAWNX = PLAYERWIDTH;
-  PLAYERJUMP = -height/9;
+  PLAYERJUMP = -height/7.5;
   world.gravity.y= height/1.5;
   wallLeft  = new Sprite(0, height/2, 0, height, 'k');
 
@@ -24,16 +24,28 @@ function setup() {
   wallBot = new Sprite(width/2, height, width, 0, 'k');
 
   player = new Sprite(PLAYERSPAWNX, PLAYERSPAWNY , PLAYERWIDTH, PLAYERHEIGHT, 'd');
+  player.color = 'red';
+  player.rotationLock =true
+  obstaclegroup = new Group();
+  obstacle = new Sprite(width-PLAYERSPAWNX,PLAYERSPAWNY , PLAYERWIDTH, PLAYERHEIGHT, 'k');
+  obstacle.color = 'blue';
+  obstacle.vel.x = -width/85;
+  obstaclegroup.add(obstacle);
 }
 
 /*******************************************************/
 // draw()
 /*******************************************************/
+function youDead(obstacle, player) {
+player.remove();
+
+}
 function draw() {
  background('green'); 
  if (kb.presses('space')&&player.y>height-height/9) {
 	player.vel.y = PLAYERJUMP;
  }
+obstacle.collides(player, youDead);
 
 }
 
